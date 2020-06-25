aaed75d9afcab303898f8f831ccef6df5368379e
Alan Nix <alan.nix@lacework.net>
2020-06-25 12:29:38 -0400

    Custom Compliance Config API
    - Implemented Custom Compliance Config API

---
+++ laceworksdk/api/custom_compliance_config.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Lacework Custom Compliance Config API wrapper.
+"""
+
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CustomComplianceConfigAPI(object):
+    """
+    Lacework Custom Compliance Config API.
+    """
+
+    def __init__(self, session):
+        """
+        Initializes the CustomComplianceConfigAPI object.
+
+        :param session: An instance of the HttpSession class.
+
+        :return CustomComplianceConfigAPI object
+        """
+
+        super(CustomComplianceConfigAPI, self).__init__()
+
+        self._session = session
+
+    def get(self):
+        """
+        A method to get the custom compliance settings from Lacework.
+
+        :return response json
+        """
+
+        logger.info("Getting custom compliance settings from Lacework...")
+
+        # Build the Run Report request URI
+        api_uri = f"/api/v1/external/CustomComplianceConfig"
+
+        response = self._session.get(api_uri)
+
+        return response.json()
+
+    def set(self, data):
+        """
+        A method to set the custom compliance settings in Lacework.
+
+        :param settings: A JSON object
+
+        :return response json
+        """
+
+        logger.info("Setting custom compliance settings in Lacework...")
+
+        # Build the Run Report request URI
+        api_uri = f"/api/v1/external/CustomComplianceConfig"
+
+        response = self._session.post(api_uri, data=data)
+
+        return response.json()
