00d554f180883141a872faa52516a1fe3cf3069b
Kristinn Gudjonsson <kristinn.gudjonsson@lacework.net>
2021-10-20 20:58:56 +0000

    Fixing a docstring.

--- jupyter/laceworkjupyter/plugins/datasource.py
+++ jupyter/laceworkjupyter/plugins/datasource.py
@@ -17,44 +17,10 @@ def process_list_data_sources(data):
 def process_datasource_schema(data):
     """
     Returns a Pandas DataFrame from the output of the API call.
+
+    :return: A pandas DataFrame.
     """
     data_dict = data.get('data', {})
     schemas = data_dict.get('resultSchema', [])
 
     return pd.DataFrame(schemas)
-
-    """
-    get_datasource_schema
-                     name              description                                       resultSchema
-0  LW_HA_DNS_REQUESTS  DNS Request information  {'name': 'BATCH_START_TIME', 'dataType': 'Time...
-
-{'data': {'name': 'LW_HA_DNS_REQUESTS',
-  'description': 'DNS Request information',
-  'resultSchema': [{'name': 'BATCH_START_TIME',
-    'dataType': 'Timestamp',
-    'description': 'Beginning of time interval'},
-   {'name': 'BATCH_END_TIME',
-    'dataType': 'Timestamp',
-    'description': 'End of time interval'},
-   {'name': 'RECORD_CREATED_TIME',
-    'dataType': 'Timestamp',
-    'description': 'Record creation time'},
-   {'name': 'MID',
-    'dataType': 'Number',
-    'description': 'Machine ID reporting the activity'},
-   {'name': 'SRV_IP_ADDR',
-    'dataType': 'String',
-    'description': 'IP address of DNS Server'},
-   {'name': 'HOSTNAME',
-    'dataType': 'String',
-    'description': 'Hostname that is being looked up'},
-   {'name': 'HOST_IP_ADDR',
-    'dataType': 'String',
-    'description': 'Resolved IP address of hostname'},
-   {'name': 'TTL',
-    'dataType': 'Number',
-    'description': 'Time to live for name resolution'},
-   {'name': 'PKTLEN',
-    'dataType': 'Number',
-    'description': 'Length of response packet'}]}}
-    """
