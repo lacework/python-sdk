4b495d7ed3bd010dc4473d7956cb22799fe68159
Kristinn Gudjonsson <kristinn.gudjonsson@lacework.net>
2021-11-19 14:08:12 +0000

    Made changes to the docker files to match changes in the code.

--- jupyter/docker/docker_build/lacework/main.js
+++ jupyter/docker/docker_build/lacework/main.js
@@ -20,10 +20,10 @@ define([
         new_cell.focus_cell();
 
         var import_cell = Jupyter.notebook.insert_cell_below('code');
-        import_cell.set_text('client = LaceworkJupyterHelper()');
+        import_cell.set_text('client = lw.get_client()');
 
         var new_cell = Jupyter.notebook.insert_cell_below('markdown');
-        new_cell.set_text('## Connecting to Lacework\nBasic imports have already been completed, we now need to connect to the Lacework API using the Jupyter helper.\nExecute the cell below by pressing the play button or using "shift + enter"');
+        new_cell.set_text('## Connecting to Lacework\nBasic imports have already been completed, we now need to connect to the Lacework API using the Jupyter helper.\nExecute the cell below by pressing the play button or using "shift + enter"\n\nAlll Lacework SDK actions happen on either the lw object or the client, which can be gathered using the lw.get_client() function.');
         new_cell.render();
         import_cell.focus_cell();
     };
