d1980611b4ce9ff682af7bf17905ea708c63a43f
Alan Nix <alan.nix@lacework.net>
2022-01-12 11:31:32 -0500

    refactor: updated HttpSession class to be much more flexible

--- laceworksdk/http_session.py
+++ laceworksdk/http_session.py
@@ -15,14 +15,15 @@ from laceworksdk import version
 from laceworksdk.config import (
     DEFAULT_BASE_DOMAIN,
     DEFAULT_ACCESS_TOKEN_EXPIRATION,
-    DEFAULT_SUCCESS_RESPONSE_CODES
+    DEFAULT_SUCCESS_RESPONSE_CODES,
+    RATE_LIMIT_RESPONSE_CODE
 )
-from laceworksdk.exceptions import ApiError
+from laceworksdk.exceptions import ApiError, MalformedResponse, RateLimitError
 
 logger = logging.getLogger(__name__)
 
 
-class HttpSession(object):
+class HttpSession:
     """
     Package HttpSession class.
     """
@@ -43,7 +44,7 @@ class HttpSession(object):
         :return HttpSession object.
         """
 
-        super(HttpSession, self).__init__()
+        super().__init__()
 
         # Create a requests session
         self._session = self._retry_session()
@@ -54,6 +55,7 @@ class HttpSession(object):
         self._base_domain = base_domain or DEFAULT_BASE_DOMAIN
         self._base_url = f"https://{account}.{self._base_domain}"
         self._subaccount = subaccount
+        self._org_level_access = False
 
         # Get an access token
         self._check_access_token()
@@ -61,7 +63,8 @@ class HttpSession(object):
     def _retry_session(self,
                        retries=3,
                        backoff_factor=0.3,
-                       status_forcelist=(500, 502, 504)):
+                       status_forcelist=(500, 502, 503, 504),
+                       allowed_methods=None):
         """
         A method to set up automatic retries on HTTP requests that fail.
         """
@@ -70,17 +73,15 @@ class HttpSession(object):
         session = requests.Session()
 
         # Establish the retry criteria
-        retry = Retry(
+        retry_strategy = Retry(
             total=retries,
-            read=retries,
-            connect=retries,
-            status=retries,
             backoff_factor=backoff_factor,
             status_forcelist=status_forcelist,
+            allowed_methods=allowed_methods
         )
 
         # Build the adapter with the retry criteria
-        adapter = HTTPAdapter(max_retries=retry)
+        adapter = HTTPAdapter(max_retries=retry_strategy)
 
         # Bind the adapter to HTTP/HTTPS calls
         session.mount("http://", adapter)
@@ -110,6 +111,8 @@ class HttpSession(object):
         """
         if response.status_code in expected_response_codes:
             pass
+        elif response.status_code == RATE_LIMIT_RESPONSE_CODE:
+            raise RateLimitError(response)
         else:
             raise ApiError(response)
 
@@ -117,6 +120,8 @@ class HttpSession(object):
         """
         Print the debug logging, based on the returned content type.
         """
+
+        logger.debug(response.headers)
 
         # If it's supposed to be a JSON response, parse and log, otherwise, log the raw text
         if "application/json" in response.headers.get("Content-Type", "").lower():
@@ -184,7 +189,7 @@ class HttpSession(object):
         headers = self._session.headers
 
         headers["Authorization"] = f"Bearer {self._access_token}"
-        headers["Org-Access"] = "true" if org_access else "false"
+        headers["Org-Access"] = "true" if self._org_level_access or org_access else "false"
         headers["User-Agent"] = f"laceworksdk-python-client/{version}"
 
         if self._subaccount:
@@ -194,141 +199,214 @@ class HttpSession(object):
 
         return headers
 
-    def get(self, uri, org=False):
+    def _request(self, method, uri, **kwargs):
         """
-        :param uri: uri to send the HTTP GET request to
-        :param org: boolean representing whether the request should be performed at the Organization level
+        A method to abstract building requests to Lacework.
+
+        :param method: string representing the HTTP request method ("GET", "POST", ...)
+        :param uri: string representing the URI of the API endpoint
+        :param kwargs: passed on to the requests package
 
         :return: response json
 
-        :raises: ApiError if unable to get a connection
+        :raises: ApiError if anything but expected response code is returned
         """
 
         self._check_access_token()
 
+        # Strip the protocol/host if provided
+        domain_begin = uri.find(self._base_domain)
+        if domain_begin >= 0:
+            domain_end = domain_begin + len(self._base_domain)
+            uri = uri[domain_end:]
+
         uri = f"{self._base_url}{uri}"
 
-        logger.info(f"GET request to URI: {uri}")
+        logger.info(f"{method} request to URI: {uri}")
+
+        # Check for 'org' - if True, make an organization-level API call
+        # TODO: Remove this on v1.0 release - this is done for back compat
+        org = kwargs.pop("org", None)
+        headers = self._get_request_headers(org_access=org)
+
+        # Check for 'data' or 'json'
+        data = kwargs.get("data", "")
+        json = kwargs.get("json", "")
+        if data or json:
+            logger.debug(f"{method} request data:\n{data}{json}")
+
+        # TODO: Remove this on v1.0 release - this is done for back compat
+        if data and not json:
+            kwargs["json"] = data
+            kwargs.pop("data")
+
+        # Make the HTTP request to the API endpoint
+        response = self._session.request(method, uri, headers=headers, **kwargs)
+
+        # Validate the response
+        self._check_response_code(response, DEFAULT_SUCCESS_RESPONSE_CODES)
+
+        self._print_debug_response(response)
+
+        # Fix for when Lacework returns a 204 with no data on searches
+        if method != "DELETE" and response.status_code == 204:
+            try:
+                response.json()
+            except Exception:
+                response._content = b'{"data": []}'
+
+        return response
+
+    def get(self, uri, params=None, **kwargs):
+        """
+        A method to build a GET request to interact with Lacework.
+
+        :param uri: uri to send the HTTP GET request to
+        :param params: dict of parameters for the HTTP request
+        :param kwargs: passed on to the requests package
+
+        :return: response json
+
+        :raises: ApiError if anything but expected response code is returned
+        """
 
         # Perform a GET request
-        response = self._session.get(uri, headers=self._get_request_headers(org_access=org))
-
-        # Validate the response
-        self._check_response_code(response, DEFAULT_SUCCESS_RESPONSE_CODES)
-
-        self._print_debug_response(response)
+        response = self._request("GET", uri, params=params, **kwargs)
 
         return response
 
-    def patch(self, uri, org=False, data=None, param=None):
+    def get_pages(self, uri, params=None, **kwargs):
         """
+        A method to build a GET request that yields pages of data returned by Lacework.
+
+        :param uri: uri to send the initial HTTP GET request to
+        :param params: dict of parameters for the HTTP request
+        :param kwargs: passed on to the requests package
+
+        :return: a generator that yields pages of data
+
+        :raises: ApiError if anything but expected response code is returned
+        """
+
+        response = self.get(uri, params=params, **kwargs)
+
+        while True:
+            yield response
+
+            try:
+                response_json = response.json()
+                next_page = response_json.get("paging", {}).get("urls", {}).get("nextPage")
+            except Exception:
+                next_page = None
+
+            if next_page:
+                response = self.get(next_page, params=params, **kwargs)
+            else:
+                break
+
+    def get_data_items(self, uri, params=None, **kwargs):
+        """
+        A method to build a GET request that yields individual objects as returned by Lacework.
+
+        :param uri: uri to send the initial HTTP GET request to
+        :param params: dict of parameters for the HTTP request
+        :param kwargs: passed on to the requests package
+
+        :return: a generator that yields individual objects from pages of data
+
+        :raises: ApiError if anything but expected response code is returned
+        :raises: MalformedResponse if the returned response does not contain a
+                top-level dictionary with an "data" key.
+        """
+
+        # Get generator for pages of JSON data
+        pages = self.get_pages(uri, params=params, **kwargs)
+
+        for page in pages:
+            page = page.json()
+            assert isinstance(page, dict)
+
+            items = page.get("data")
+
+            if items is None:
+                error_message = f"'data' key not found in JSON data:\n{page}"
+                raise MalformedResponse(error_message)
+
+            for item in items:
+                yield item
+
+    def patch(self, uri, data=None, json=None, **kwargs):
+        """
+        A method to build a PATCH request to interact with Lacework.
+
         :param uri: uri to send the HTTP POST request to
-        :param org: boolean representing whether the request should be performed at the Organization level
-        :param data: json object containing the data
-        :param param: python object containing the parameters
+        :param data: data to be sent in the body of the request
+        :param json: data to be sent in JSON format in the body of the request
+        :param kwargs: passed on to the requests package
 
         :return: response json
 
-        :raises: ApiError if unable to get a connection
+        :raises: ApiError if anything but expected response code is returned
         """
-
-        self._check_access_token()
-
-        uri = f"{self._base_url}{uri}"
-
-        logger.info(f"PATCH request to URI: {uri}")
-        logger.debug(f"PATCH request data:\n{data}")
 
         # Perform a PATCH request
-        response = self._session.patch(uri, params=param, json=data, headers=self._get_request_headers(org_access=org))
-
-        # Validate the response
-        self._check_response_code(response, DEFAULT_SUCCESS_RESPONSE_CODES)
-
-        self._print_debug_response(response)
+        response = self._request("PATCH", uri, data=data, json=json, **kwargs)
 
         return response
 
-    def post(self, uri, org=False, data=None, param=None):
+    def post(self, uri, data=None, json=None, **kwargs):
         """
+        A method to build a POST request to interact with Lacework.
+
         :param uri: uri to send the HTTP POST request to
-        :param org: boolean representing whether the request should be performed at the Organization level
-        :param data: json object containing the data
-        :param param: python object containing the parameters
+        :param data: data to be sent in the body of the request
+        :param json: data to be sent in JSON format in the body of the request
+        :param kwargs: passed on to the requests package
 
         :return: response json
 
-        :raises: ApiError if unable to get a connection
+        :raises: ApiError if anything but expected response code is returned
         """
-
-        self._check_access_token()
-
-        uri = f"{self._base_url}{uri}"
-
-        logger.info(f"POST request to URI: {uri}")
-        logger.debug(f"POST request data:\n{data}")
 
         # Perform a POST request
-        response = self._session.post(uri, params=param, json=data, headers=self._get_request_headers(org_access=org))
-
-        # Validate the response
-        self._check_response_code(response, DEFAULT_SUCCESS_RESPONSE_CODES)
-
-        self._print_debug_response(response)
+        response = self._request("POST", uri, data=data, json=json, **kwargs)
 
         return response
 
-    def put(self, uri, org=False, data=None, param=None):
+    def put(self, uri, data=None, json=None, **kwargs):
         """
+        A method to build a PUT request to interact with Lacework.
+
         :param uri: uri to send the HTTP POST request to
-        :param org: boolean representing whether the request should be performed at the Organization level
-        :param data: json object containing the data
-        :param param: python object containing the parameters
+        :param data: data to be sent in the body of the request
+        :param json: data to be sent in JSON format in the body of the request
+        :param kwargs: passed on to the requests package
 
         :return: response json
 
-        :raises: ApiError if unable to get a connection
+        :raises: ApiError if anything but expected response code is returned
         """
 
-        self._check_access_token()
-
-        uri = f"{self._base_url}{uri}"
-
-        logger.info(f"PUT request to URI: {uri}")
-        logger.debug(f"PUT request data:\n{data}")
-
         # Perform a PUT request
-        response = self._session.put(uri, params=param, json=data, headers=self._get_request_headers(org_access=org))
-
-        # Validate the response
-        self._check_response_code(response, DEFAULT_SUCCESS_RESPONSE_CODES)
-
-        self._print_debug_response(response)
+        response = self._request("PUT", uri, data=data, json=json, **kwargs)
 
         return response
 
-    def delete(self, uri, data=None, org=False):
+    def delete(self, uri, data=None, json=None, **kwargs):
         """
+        A method to build a DELETE request to interact with Lacework.
+
         :param uri: uri to send the http DELETE request to
-        :param org: boolean representing whether the request should be performed at the Organization level
+        :param data: data to be sent in the body of the request
+        :param json: data to be sent in JSON format in the body of the request
+        :param kwargs: passed on to the requests package
 
         :response: reponse json
 
-        :raises: ApiError if unable to get a connection
+        :raises: ApiError if anything but expected response code is returned
         """
 
-        self._check_access_token()
-
-        uri = f"{self._base_url}{uri}"
-
-        logger.info(f"DELETE request to URI: {uri}")
-
         # Perform a DELETE request
-        response = self._session.delete(uri, json=data, headers=self._get_request_headers(org_access=org))
-
-        # Validate the response
-        self._check_response_code(response, DEFAULT_SUCCESS_RESPONSE_CODES)
-
-        self._print_debug_response(response)
+        response = self._request("DELETE", uri, data=data, json=json, **kwargs)
 
         return response
