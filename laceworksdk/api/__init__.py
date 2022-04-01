435b2766849fef3e1aa28896cc378e7a25d2cf46
Alan Nix <alan.nix@lacework.net>
2022-04-01 14:22:07 -0400

    feat: adding vulnerability policies support

--- laceworksdk/api/__init__.py
+++ laceworksdk/api/__init__.py
@@ -46,6 +46,7 @@ from .v2.team_members import TeamMembersAPI
 from .v2.user_profile import UserProfileAPI
 from .v2.vulnerabilities import VulnerabilitiesAPI
 from .v2.vulnerability_exceptions import VulnerabilityExceptionsAPI
+from .v2.vulnerability_policies import VulnerabilityPoliciesAPI
 
 from laceworksdk.config import (
     LACEWORK_ACCOUNT_ENVIRONMENT_VARIABLE,
@@ -167,6 +168,7 @@ class LaceworkClient:
         self.user_profile = UserProfileAPI(self._session)
         self.vulnerabilities = VulnerabilitiesAPI(self._session)
         self.vulnerability_exceptions = VulnerabilityExceptionsAPI(self._session)
+        self.vulnerability_policies = VulnerabilityPoliciesAPI(self._session)
 
     def set_org_level_access(self, org_level_access):
         """
