fb3b68e05eced14592c0ccb89e94b47f460af8c3
Alan Nix <65611624+alannix-lw@users.noreply.github.com>
2022-08-02 20:52:25 -0400

    feat: Implemented new Alerts API endpoints (#74)

--- laceworksdk/api/v2/alerts.py
+++ laceworksdk/api/v2/alerts.py
@@ -46,11 +46,14 @@ class AlertsAPI(SearchEndpoint):
 
     def get_details(self,
                     id,
+                    scope,
                     **request_params):
         """
         A method to get Alerts objects by ID.
 
         :param id: A string representing the object ID.
+        :param scope: A string representing the scope of the detailst to return.
+            ("Details", "Investigation", "Events", "RelatedAlerts", "Integrations", "Timeline")
         :param request_params: Additional request parameters.
             (provides support for parameters that may be added in the future)
 
@@ -58,7 +61,8 @@ class AlertsAPI(SearchEndpoint):
         """
 
         params = self.build_dict_from_items(
-            request_params
+            request_params,
+            scope=scope
         )
 
         response = self._session.get(self.build_url(id=id), params=params)
@@ -77,3 +81,51 @@ class AlertsAPI(SearchEndpoint):
         """
 
         return super().search(json=json)
+
+    def comment(self,
+                id,
+                comment):
+        """
+        A method to comment on an Alerts object.
+
+        :param id: A string representing the object ID.
+        :param comment: A string representing the comment to post.
+
+        :return response json
+        """
+
+        json = self.build_dict_from_items(
+            comment=comment
+        )
+
+        response = self._session.post(self.build_url(resource=id, action="comment"), json=json)
+
+        return response.json()
+
+    def close(self,
+              id,
+              reason,
+              comment=None):
+        """
+        A method to close an Alerts object.
+
+        :param id: A string representing the object ID.
+        :param reason: An integer representing the close reason.
+            0: Other
+            1: False positive
+            2: Not enough information
+            3: Malicious and have resolution in place
+            4: Expected because of routine testing
+        :param comment: A string representing the comment to post.
+
+        :return response json
+        """
+
+        json = self.build_dict_from_items(
+            reason=reason,
+            comment=comment
+        )
+
+        response = self._session.post(self.build_url(resource=id, action="close"), json=json)
+
+        return response.json()
