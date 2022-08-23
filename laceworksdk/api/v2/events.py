063968a4cbbfa89198a63ec843e6e97a1396771a
Alan Nix <65611624+alannix-lw@users.noreply.github.com>
2022-08-22 21:29:31 -0400

    chore: linting corrections for the EventsAPIv2 (#83)

--- laceworksdk/api/v2/events.py
+++ laceworksdk/api/v2/events.py
@@ -4,7 +4,8 @@ Lacework Events API wrapper.
 """
 
 from laceworksdk.api.search_endpoint import SearchEndpoint
-from laceworksdk.api.v1.events import EventsAPIv1 
+from laceworksdk.api.v1.events import EventsAPIv1
+
 
 class EventsAPIv2(EventsAPIv1, SearchEndpoint):
 
@@ -20,5 +21,4 @@ class EventsAPIv2(EventsAPIv1, SearchEndpoint):
         # The need to pass "Events" into the v1 init() is tied
         # to the super() call within the v1 init()
         super(EventsAPIv1, self).__init__(session, "Events")
-
         super(SearchEndpoint, self).__init__(session, "Events")
