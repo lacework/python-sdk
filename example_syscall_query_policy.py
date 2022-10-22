2b2d64ad14fdcba40ac10654e018c6c3d0616180
christopher hall <61752217+lw-chall@users.noreply.github.com>
2022-10-22 12:15:11 -0400

    fixed alert profiles

--- example_syscall_query_policy.py
+++ example_syscall_query_policy.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         remediation="Policy remediation here..",
         severity="high",
         alert_enabled=True,
-        #alert_profile="LW_HA_SYSCALLS_EXEC_DEFAULT_PROFILE",
-        #alert_profile="LW_HE_SYSCALLS_PROCESSES_DEFAULT_PROFILE",
+        #alert_profile="LW_HA_SYSCALLS_EXEC_DEFAULT_PROFILE.Violation",
+        #alert_profile="LW_HE_SYSCALLS_PROCESSES_DEFAULT_PROFILE.Violation",
         alert_profile="LW_HA_SYSCALLS_FILE_DEFAULT_PROFILE.Violation"
     )
