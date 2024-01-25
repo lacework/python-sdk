7053e315651f59963de012033115b440fd526c8f
Kristinn Gudjonsson <kristinn.gudjonsson@lacework.net>
2024-01-25 12:48:05 +0000

    more updates

--- jupyter/laceworkjupyter/plugins/__init__.py
+++ jupyter/laceworkjupyter/plugins/__init__.py
@@ -1,8 +1,6 @@
 """A simple loading of plugins."""
 
 from . import alerts
-from . import events
-from . import evidence
 from . import alert_rules
 from . import datasources
 
@@ -12,6 +10,4 @@ PLUGINS = {
     'alert_rules.get': alert_rules.process_alert_rules,
     'datasources.list_data_sources': datasources.process_list_data_sources,
     'datasources.get_datasource': datasources.process_datasource_schema,
-    'events.get_details': events.process_event_details,
-    'evidence.search': evidence.process_evidence_search,
 }
